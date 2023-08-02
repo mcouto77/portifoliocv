5347b1ca8ab22231dadce2fa7e874408a719a1d2
MICHEL COUTO DO AMPARO <mcouto77@yahoo.com.br>
2023-08-02 08:57:04 -0300

    comite parte 5

--- portifolio/src/Routes.jsx
+++ portifolio/src/Routes.jsx
@@ -1,22 +1,23 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Page404 from './pages/Page404'
-import Home from './pages/Home';
-import Sobre from './pages/Sobre';
-import Projetos from './pages/Projetos';
-import Contatos from './pages/Contatos';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Sobre from "./pages/Sobre";
+import Projetos from "./pages/Projetos";
+import Contatos from "./pages/Contatos";
+import Page404 from "./pages/Page404";
+import PageBase from "./pages/PageBase";
 
 function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} ></Route>
-                <Route path="/sobre" element={<Sobre />} ></Route>
-                <Route path="/projetos" element={<Projetos />} ></Route>
-                <Route path="/contatos" element={<Contatos />} ></Route>
-                <Route path="*" element={ <Page404 /> }></Route>
-            </Routes>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PageBase />}>
+          <Route index element={<Home />}></Route>
+          <Route path="/sobre" element={<Sobre />}></Route>
+          <Route path="/projetos" element={<Projetos />}></Route>
+          <Route path="/contatos" element={<Contatos />}></Route>
+          <Route path="*" element={<Page404 />}></Route>
+        </Route>
+      </Routes>
     </BrowserRouter>
-    )
+  );
 }
-
-export default AppRoutes
--- portifolio/src/pages/Home/index.jsx
+++ portifolio/src/pages/Home/index.jsx
@@ -1,32 +1,23 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 import { Link } from "react-router-dom";
-import Container from "../../components/Container";
 import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <>
-      <Header />
-      <Container>
-        <section className={styles.home}>
-          <div className={styles.apresentacao}>
-            <p>
-              Olá, sou <br />
-              <span>Michel Couto</span> <br />
-              Dev Full Stack
-            </p>
-            <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
-              Saiba mais sobre mim
-            </Link>
-          </div>
-          <figure>
-            <img className={styles.img_home} src="/developer.svg" alt="Imagem de Home" />
-          </figure>
-        </section>
-      </Container>
-      <Footer />
-    </>
+    <section className={styles.home}>
+      <div className={styles.apresentacao}>
+        <p>
+          Olá, sou <br />
+          <span>Michel Couto</span> <br />
+          Dev Full Stack
+        </p>
+        <Link to="/sobre" className={`${styles.btn} ${styles.btn_red}`}>
+          Saiba mais sobre mim
+        </Link>
+      </div>
+      <figure>
+        <img className={styles.img_home} src="/developer.svg" alt="Imagem de Home" />
+      </figure>
+    </section>
   );
 }
 
