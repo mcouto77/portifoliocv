e20393f8237e3ed092c6b4836d6063c4068d4155
MICHEL COUTO DO AMPARO <mcouto77@yahoo.com.br>
2023-08-02 20:18:13 -0300

    comite 5.1

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,10 +1,9 @@
-import "./App.css";
-import AppRoutes from './Routes'
+import AppRoutes from './routes'
 
 function App() {
-  return
-   (
-   <AppRoutes />
+
+  return (
+    <AppRoutes />
   )
 }
 
